0c641676b9550fa00d9d94cfc2c0dac27aa93f35
Abdulrahman Alyahya <al_yahya179@hotmail.com>
2023-02-13 05:53:37 +0300

    Add product components

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  backendError: boolean = false;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProductList().subscribe({
+      next: res => {
+        this.productService.setProducts(res);
+        this.backendError = false;
+        this.products = this.productService.getProducts();
+      },
+      error: error => {
+        console.error('Error getting product list: ' + JSON.stringify(error));
+        this.productService.setProducts([]);
+        this.backendError = true;
+        this.products = [];
+      }
+    });
+  }
 }
